85542b92fddf6b8e325b6d14122f814c9b099236
huangchangjun <huangchangjun01@gmail.com>
2026-07-28 16:03:01 +0800

    rollback

--- frontend/src/types/v-md-editor.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@kangc/v-md-editor' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<any, any, any>
-  export default component
-}
-
-declare module '@kangc/v-md-editor/lib/style/base-editor.css'
-declare module '@kangc/v-md-editor/lib/theme/style/vuepress.css'
